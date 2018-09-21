b1d4afccc8829fa6d474084708c435af3f9fae0c
Muge <36175044+MugeHasilci@users.noreply.github.com>
2018-09-21 16:46:00 +0100

    Add files via upload

---
+++ User-Input/username_password.rb
@@ -0,0 +1,24 @@
+=begin
+In the previous exercise, you wrote a program to solicit a password. In this exercise,
+you should modify the program so it also requires a user name. The program should solicit
+both the user name and the password, then validate both, and issue a generic error message
+if one or both are incorrect; the error message should not tell the user which item is
+incorrect.
+=end
+
+username = 'admin'
+password = 'SecreT'
+
+loop do
+puts "Please enter user name:"
+username = gets.chomp.to_i
+puts "Please enter your password:"
+password = gets.chomp.to_i
+
+if username != "admin" || password != "SecreT"
+  puts "Authorization failed!"
+  end
+  break
+end
+
+puts "Welcome!"
